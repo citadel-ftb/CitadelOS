b675f6114c5fc0edd507dfebf4375ccd36e00924
Margaret Dax <mollrow@gmail.com>
2020-04-13 20:31:07 -0500

    Fix issue with gps_ext module exporting

--- citadel_rom/apis/gps_ext.lua
+++ citadel_rom/apis/gps_ext.lua
@@ -1,60 +1,6 @@
 local gps_ext = {}
 
-CHANNEL_GPS = 65534
-
-local function trilaterate( A, B, C )
-    local a2b = B.vPosition - A.vPosition
-    local a2c = C.vPosition - A.vPosition
-
-    if math.abs( a2b:normalize():dot( a2c:normalize() ) ) > 0.999 then
-        return nil
-    end
-
-    local d = a2b:length()
-    local ex = a2b:normalize( )
-    local i = ex:dot( a2c )
-    local ey = (a2c - (ex * i)):normalize()
-    local j = ey:dot( a2c )
-    local ez = ex:cross( ey )
-
-    local r1 = A.nDistance
-    local r2 = B.nDistance
-    local r3 = C.nDistance
-
-    local x = (r1*r1 - r2*r2 + d*d) / (2*d)
-    local y = (r1*r1 - r3*r3 - x*x + (x-i)*(x-i) + j*j) / (2*j)
-
-    local result = A.vPosition + (ex * x) + (ey * y)
-
-    local zSquared = r1*r1 - x*x - y*y
-    if zSquared > 0 then
-        local z = math.sqrt( zSquared )
-        local result1 = result + (ez * z)
-        local result2 = result - (ez * z)
-
-        local rounded1, rounded2 = result1:round( 0.01 ), result2:round( 0.01 )
-        if rounded1.x ~= rounded2.x or rounded1.y ~= rounded2.y or rounded1.z ~= rounded2.z then
-            return rounded1, rounded2
-        else
-            return rounded1
-        end
-    end
-    return result:round( 0.01 )
-
-end
-
-local function narrow( p1, p2, fix )
-    local dist1 = math.abs( (p1 - fix.vPosition):length() - fix.nDistance )
-    local dist2 = math.abs( (p2 - fix.vPosition):length() - fix.nDistance )
-
-    if math.abs(dist1 - dist2) < 0.01 then
-        return p1, p2
-    elseif dist1 < dist2 then
-        return p1:round( 0.01 )
-    else
-        return p2:round( 0.01 )
-    end
-end
+gps_ext.CHANNEL_GPS = 65534
 
 function gps_ext.locate_hosts( _nTimeout, _bDebug )
     -- Find a modem
@@ -75,7 +21,7 @@ function gps_ext.locate_hosts( _nTimeout, _bDebug )
     end
 
     -- Send a ping to listening GPS hosts
-    modem.transmit( CHANNEL_GPS, os.getComputerID(), "PING" )
+    modem.transmit( gps_ext.CHANNEL_GPS, os.getComputerID(), "PING" )
 
     -- Wait for the responses
     local hosts = {}
@@ -85,7 +31,7 @@ function gps_ext.locate_hosts( _nTimeout, _bDebug )
         if e == "modem_message" then
             -- We received a reply from a modem
             local sSide, sChannel, sReplyChannel, tMessage, nDistance = p1, p2, p3, p4, p5
-            if sSide == sModemSide and sChannel == os.getComputerID() and sReplyChannel == CHANNEL_GPS and nDistance then
+            if sSide == sModemSide and sChannel == os.getComputerID() and sReplyChannel == gps_ext.CHANNEL_GPS and nDistance then
                 -- Received the correct message from the correct modem: use it to determine position
                 if type(tMessage) == "table" and #tMessage == 3 and tonumber(tMessage[1]) and tonumber(tMessage[2]) and tonumber(tMessage[3]) then
                     local host = vector.new( tMessage[1], tMessage[2], tMessage[3] )
@@ -113,4 +59,4 @@ function gps_ext.locate_hosts( _nTimeout, _bDebug )
     return hosts
 end
 
-return extgps
+return gps_ext
